99314b6578fc077d77183278c090e96dfbba4874
Tim Long <timuism@me.com>
2023-04-30 01:27:56 -0400

    remove loop

--- pages/episode/[...id].vue
+++ pages/episode/[...id].vue
@@ -4,14 +4,14 @@ import { EpisodeData } from '~/types'
 const route = useRoute()
 const query = groq`*[_id == "${route.params.id[0]}"][0]`
 const { data, refresh } = useLazySanityQuery(query)
-const episodes = data.value as EpisodeData[]
-console.log({ episodes })
+const episode = data.value as EpisodeData
+console.log({ episode })
 </script>
 <template>
-  <div class="w-full max-w-screen-2xl mx-auto p-24">
-    <article v-for="episode in episodes"
+  <div v-if="episode" class="w-full max-w-screen-2xl mx-auto p-24">
+    <article
       class="group outline-none rounded-lg bg-transparent duration-700 p-12 grid grid-cols-4 gap-20">
-      <div class="col-span-1">
+      <div v-if="episode.epImage" class="col-span-1">
         <div class="rounded-md overflow-hidden shadow-flat h-[200px]">
           <!-- <img src="/stockphoto1.jpg" alt="" class="object-cover object-top w-full h-full" /> -->
           <SanityImage :asset-id="episode.epImage.asset._ref" alt="" class="object-cover object-top w-full h-full" />
